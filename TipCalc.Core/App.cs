4252692b99dde014607082845b21e135445171fb
Yasuo Okamoto <jack.jilljp2@gmail.com>
2018-11-24 14:56:56 +0900

    チュートリアルSTEP1終了

---
+++ TipCalc.Core/App.cs
@@ -0,0 +1,22 @@
+﻿using MvvmCross;
+using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TipCalc.Core.Services;
+using TipCalc.Core.ViewModels;
+
+namespace TipCalc.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            //  IoC ContainerへICalculationServiceへの参照をどこからでも要求できるようにする
+            Mvx.IoCProvider.RegisterType<ICalculationService, CalculationService>();
+
+            // アプリケーションの起動時に表示するViewに必要なViewModelを登録
+            RegisterAppStart<TipViewModel>();
+        }
+    }
+}
---
+++ TipCalc.Core/Services/CalculationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipCalc.Core.Services
+{
+    public class CalculationService : ICalculationService
+    {
+        public double TipAmount(double subTotal, int generousity)
+        {
+            return subTotal * ((double)generousity) / 100.0;
+        }
+    }
+}
